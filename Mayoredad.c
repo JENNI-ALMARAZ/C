0a855a0379db2ab35f8776524993c45313427b87
JENNI-ALMARAZ <91512122+JENNI-ALMARAZ@users.noreply.github.com>
2021-10-19 16:01:07 -0500

    RESOLUCIÓN DE UN ALGORITMO EN LENGUAJE C
    De acuerdo a nuestro año de nacimiento determinamos si somos mayor o menor de edad.

---
+++ Mayoredad.c
@@ -0,0 +1,28 @@
+/* 
+Autor: Jennifer ALmaraz Suárez 
+Fecha: 19_10_2021
+*/
+
+/*Realizar un código que determine si el usuario es mayor o menor de edad dependiendo del año de nacimiento*/
+
+#include <stdio.h>
+
+int main(){//Inicio metodo principal
+
+	//Declaracion de variables 
+	int fecha_Nacimiento; 
+	int fecha_Actual = 2021;
+	int diferencia = 0;
+
+ 	printf ("Digite su año de nacimiento: ");
+	scanf ("%d", &fecha_Nacimiento);
+
+	diferencia = (fecha_Actual - fecha_Nacimiento);
+
+	if ( diferencia >= 18){
+		printf ("%s %d %s", "El usuario tiene ", diferencia, "por lo tanto es mayor de edad");
+	}
+	else {
+		printf("%s %d %s", "El usuario tiene", diferencia, "por lo tanto es menor de edad");
+	} 
+}//Fin metodo principal 
